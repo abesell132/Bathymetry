11a842c1c2cb26fabc9f89d23d7a65da2810c797
Abe <abesell132@gmail.com>
2021-12-03 19:16:26 -0500

    asdf

--- NeuralNetwork/tensorNetwork.js
+++ NeuralNetwork/tensorNetwork.js
@@ -1,16 +1,31 @@
 const tf = require("@tensorflow/tfjs-node-gpu");
 
-let model = tf.sequential();
+// let model = tf.sequential();
 
-let hidden = tf.layers.dense({
-  units: 10,
-  activation: "sigmoid",
-  inputDim: 51,
-});
-let output = tf.layers.dense({
-  units: 4,
-  activation: "softmax",
-});
+// let hidden = tf.layers.dense({
+//   units: 10,
+//   activation: "sigmoid",
+//   inputDim: 51,
+// });
+// let output = tf.layers.dense({
+//   units: 4,
+//   activation: "softmax",
+// });
 
-model.add(hidden);
-model.add(output);
+// model.add(hidden);
+// model.add(output);
+
+const fs = require("fs")
+
+class TensorNetwork {
+  constructor(inputs, targets) {
+    this.inputs = inputs;
+    this.targets = targets;
+
+    let file = fs.readFileSync("../trainingData/trainingData.json")
+    this.trainingData = JSON.parse(file);
+    console.log(this.trainingData)
+  }
+}
+
+module.exports = TensorNetwork;
--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const training = require("./trainingData/trainingData");
-const NeuralNetwork = require("./NeuralNetwork");
+const fs = require("fs");
 
 async function start() {
   try {
     let trainingCoords = await training.getTraningCoords();
     let baseImg = await training.getBaseImg();
-    let nn = new NeuralNetwork(51, [27, 13], 4);
 
-    let trainingCoord = await training.get_random_training_data(trainingCoords);
-    let trainingPixelValues = await training.findBasePixelValues(trainingCoord, baseImg);
-    let expectedResult = await training.getExpectedResult(trainingCoord.type);
+    let trainingData = { values: [] };
+    for (let a = 0; a < 10000; a++) {
+      let trainingCoord = await training.get_random_training_data(trainingCoords);
+      let trainingPixelValues = await training.findBasePixelValues(trainingCoord, baseImg);
 
-    await console.time("Training");
+      trainingData.values.push({
+        inputs: trainingPixelValues,
+        outputs: trainingCoord.type,
+      });
+    }
 
-    await nn.train(trainingPixelValues, expectedResult);
-    await console.timeEnd("Training");
+    await fs.writeFile("./trainingData/trainingData.json", JSON.stringify(trainingData), function (err) {
+      if (err) return console.log(err);
+    });
   } catch (e) {
     console.log(e);
   }
