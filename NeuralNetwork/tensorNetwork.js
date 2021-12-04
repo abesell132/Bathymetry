47389850c1da3c7eb900ea8218c2d81ad95ca668
Abe <abesell132@gmail.com>
2021-12-03 19:47:53 -0500

    asdf

--- NeuralNetwork/tensorNetwork.js
+++ NeuralNetwork/tensorNetwork.js
@@ -1,4 +1,82 @@
 const tf = require("@tensorflow/tfjs-node-gpu");
+const fs = require("fs");
+
+let labelList = ["land", "water", "depthLine", "depthNumber"];
+
+let trainingData = JSON.parse(fs.readFileSync("../trainingData/trainingData.json"));
+
+let colors = [];
+let labels = [];
+for (let record of trainingData.values) {
+  console.log(labelList.indexOf(record.outputs));
+
+  colors.push(record.inputs);
+  labels.push(labelList.indexOf(record.outputs));
+}
+
+let xs = tf.tensor2d(colors);
+let labelsTensor = tf.tensor1d(labels, "int32");
+
+let ys = tf.oneHot(labelsTensor, 4).cast("float32");
+labelsTensor.dispose();
+
+model = buildModel();
+
+let model = tf.sequential();
+const hidden = tf.layers.dense({
+  units: 15,
+  inputShape: [3],
+  activation: "sigmoid",
+});
+
+const output = tf.layers.dense({
+  units: 9,
+  activation: "softmax",
+});
+model.add(hidden);
+model.add(output);
+
+const LEARNING_RATE = 0.25;
+const optimizer = tf.train.sgd(LEARNING_RATE);
+
+model.compile({
+  optimizer: optimizer,
+  loss: "categoricalCrossentropy",
+  metrics: ["accuracy"],
+});
+
+let isTraining = false;
+
+train();
+
+async function train() {
+  if (istraining) {
+    return;
+  }
+  istraining = true;
+  // This is leaking https://github.com/tensorflow/tfjs/issues/457
+  await model.fit(xs, ys, {
+    shuffle: true,
+    validationSplit: 0.1,
+    epochs: 10,
+    callbacks: {
+      onEpochEnd: (epoch, logs) => {
+        console.log(epoch);
+        lossY.push(logs.val_loss.toFixed(2));
+        accY.push(logs.val_acc.toFixed(2));
+        lossX.push(lossX.length + 1);
+        lossP.html("Loss: " + logs.loss.toFixed(5));
+      },
+      onBatchEnd: async (batch, logs) => {
+        await tf.nextFrame();
+      },
+      onTrainEnd: () => {
+        istraining = false;
+        console.log("finished");
+      },
+    },
+  });
+}
 
 // let model = tf.sequential();
 
@@ -15,17 +93,17 @@ const tf = require("@tensorflow/tfjs-node-gpu");
 // model.add(hidden);
 // model.add(output);
 
-const fs = require("fs")
+// const fs = require("fs");
 
-class TensorNetwork {
-  constructor(inputs, targets) {
-    this.inputs = inputs;
-    this.targets = targets;
+// class TensorNetwork {
+//   constructor(inputs, targets) {
+//     this.inputs = inputs;
+//     this.targets = targets;
 
-    let file = fs.readFileSync("../trainingData/trainingData.json")
-    this.trainingData = JSON.parse(file);
-    console.log(this.trainingData)
-  }
-}
+//     let file = fs.readFileSync("../trainingData/trainingData.json");
+//     this.trainingData = JSON.parse(file);
+//     console.log(this.trainingData);
+//   }
+// }
 
-module.exports = TensorNetwork;
+// module.exports = TensorNetwork;
